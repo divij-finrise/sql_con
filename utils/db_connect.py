1052866c92492d210b475bf1072a4b2b968e8256
divij-finrise <divij.finrisesoftech@gmail.com>
2023-06-14 17:15:41 +0530

    implemented class structure in db_connect

--- utils/db_connect.py
+++ utils/db_connect.py
@@ -3,50 +3,51 @@ import sys
 import pyodbc
 from utils.util import config
 
-def db_connection_src():
-    '''Source server connection details'''
-    server = config("src_ip")
-    username = config("src_username")
-    password = config("src_password")
-    driver = config("src_driver")
-    try:
-        cnxn = pyodbc.connect(
-            "DRIVER="
-            + driver
-            + ";SERVER="
-            + server
-            + ";UID="
-            + username
-            + ";PWD="
-            + password
-            + ";TrustServerCertificate=yes"
-        )
-        return cnxn
+class DatabaseConnection:
+    def __init__(self):
+        self.src_server = config("src_ip")
+        self.src_username = config("src_username")
+        self.src_password = config("src_password")
+        self.driver = config("driver")
+        self.dst_server = config("dst_ip")
+        self.dst_username = config("dst_username")
+        self.dst_password = config("dst_password")
 
-    except Exception as e:
-        print("[Error] in (db_connect,db_connection_src) msg: ", str(e))
-        sys.exit(1)
 
-def db_connection_dst():
-    '''Destination server connection details'''
-    server = config("dst_ip")
-    username = config("dst_username")
-    password = config("dst_password")
-    driver = config("dst_driver")
-    try:
-        cnxn = pyodbc.connect(
-            "DRIVER="
-            + driver
-            + ";SERVER="
-            + server
-            + ";UID="
-            + username
-            + ";PWD="
-            + password
-            + ";TrustServerCertificate=yes"
-        )
-        return cnxn
+    def db_connection_src(self):
+        try:
+            cnxn = pyodbc.connect(
+                "DRIVER="
+                + self.driver
+                + ";SERVER="
+                + self.src_server
+                + ";UID="
+                + self.src_username
+                + ";PWD="
+                + self.src_password
+                + ";TrustServerCertificate=yes"
+            )
+            return cnxn
 
-    except Exception as e:
-        print("[Error] in (db_connect,db_connection_dst) msg: ", str(e))
-        sys.exit(1)
+        except Exception as e:
+            print("[Error] in (db_connect,db_connection_src) msg: ", str(e))
+            sys.exit(1)
+
+    def db_connection_dst(self):
+        try:
+            cnxn = pyodbc.connect(
+                "DRIVER="
+                + self.driver
+                + ";SERVER="
+                + self.dst_server
+                + ";UID="
+                + self.dst_username
+                + ";PWD="
+                + self.dst_password
+                + ";TrustServerCertificate=yes"
+            )
+            return cnxn
+
+        except Exception as e:
+            print("[Error] in (db_connect,db_connection_dst) msg: ", str(e))
+            sys.exit(1)
